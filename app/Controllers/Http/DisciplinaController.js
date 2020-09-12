ad1f2f7bb94f59b196a70ea335830b5bfb96e6fb
Bebeto Alves <bebeto.contato@gmail.com>
2020-09-11 23:47:58 -0300

    Refatoração do teste de disciplinas

--- app/Controllers/Http/DisciplinaController.js
+++ app/Controllers/Http/DisciplinaController.js
@@ -7,7 +7,7 @@ const { validateAll } = use("Validator");
 class DisciplinaController {
 
   async store({ request, response }) {
-    try{
+    try {
       const errorMessage = {
         'nome.required': 'É obrigatório um nome para a Disciplina',
         'curso_id.required': 'É obrigatório um id do Curso'
@@ -21,7 +21,7 @@ class DisciplinaController {
         errorMessage
       )
 
-      if(validation.fails()){
+      if (validation.fails()) {
         return response.status(400).send({ message: validation.messages() });
       }
 
@@ -34,7 +34,7 @@ class DisciplinaController {
 
       return response.status(201).send(disciplina)
 
-    }catch (error){
+    } catch (error) {
       return response.status(500).send({ erro: `Erro: ${error.message}` });
     }
   }
@@ -42,10 +42,10 @@ class DisciplinaController {
 
 
   async destroy({ params, response }) {
-    try{
+    try {
       const disciplina = await Disciplina.query().where("id", params.id).first();
 
-      if(!disciplina){
+      if (!disciplina) {
         return response.status(400).send({ message: "Disciplina não existe" });
       }
 
@@ -60,18 +60,17 @@ class DisciplinaController {
 
 
 
-  async update({ params, request, response  }){
+  async update({ params, request, response }) {
     try {
-      const {nome} = request.only(['nome']);
+      const data = request.only(['nome', 'curso_id']);
 
-      const disciplina = await Disciplina.query().where("id", params.id).first();
+      const disciplina = await Disciplina.findOrFail(params.id);
 
-      if(!disciplina){
+      if (!disciplina) {
         return response.status(400).send({ message: "Disciplina não existe" });
       }
 
-      disciplina.nome = nome;
-      disciplina.id = params.id;
+      disciplina.merge(data);
 
       await disciplina.save();
 
@@ -85,11 +84,11 @@ class DisciplinaController {
 
 
 
-  async show({ params, response }){
+  async show({ params, response }) {
     try {
       const disciplina = await Disciplina.query().where("id", params.id).first();
 
-      if(!disciplina){
+      if (!disciplina) {
         return response.status(400).send({ message: "Disciplina não existe" });
       }
 
@@ -102,7 +101,7 @@ class DisciplinaController {
 
 
 
-  async index({ request, response }){
+  async index({ request, response }) {
     try {
       const disciplina = await Disciplina.all();
 
--- test/functional/disciplina.spec.js
+++ test/functional/disciplina.spec.js
@@ -1,66 +1,68 @@
 const { test, trait } = use('Test/Suite')('Disciplina')
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Disciplina = use('App/Models/Disciplina')
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use("Factory");
 
-trait("Test/ApiClient")
+trait('Test/ApiClient')
+trait('DatabaseTransactions')
 
-test("Uma disciplina pode ser apagada", async ({ assert, client }) => {
-  const disciplina = await Disciplina.create({
-    nome: 'Português',
-    curso_id: 1
-  })
+test("uma disciplina pode ser criada", async ({ assert, client }) => {
+  const { nome, curso_id } = await Factory.model('App/Models/Disciplina').make();
 
-  const response = await client.delete('/api/disciplinas/' + disciplina.id).end()
+  const response = await client.post('/api/disciplinas')
+    .send({ nome, curso_id })
+    .end()
 
-  response.assertStatus(200)
-
+  response.assertStatus(201);
 });
 
-test("Uma disciplina pode ser criada", async ({ assert, client }) => {
-  const disciplina = await Disciplina.create({
-    nome: 'Matemática',
-    curso_id: 1
-  })
+test("as disciplinas podem ser visualizadas", async ({ assert, client }) => {
+  const disciplinas = await Factory.model('App/Models/Disciplina').createMany(3);
 
-  const response = await client.post('/api/disciplinas').send({
-    nome: 'Matemática',
-    curso_id: 1
-  })
-  .end()
+  const allDisciplinas = await Disciplina.all();
 
-  response.assertStatus(201)
+  const response = await client.get('/api/disciplinas').end();
+
+  response.assertStatus(200);
+  response.assertJSON(allDisciplinas.toJSON());
 });
 
-test("As disciplinas podem ser visualizadas", async ({ assert, client }) => {
-  const disciplina = await Disciplina.create({
-    nome: 'Matemática',
-    curso_id: 1
-  })
-
-  const response = await client.get('/api/disciplinas').end()
-
-  response.assertStatus(200)
-});
-
-test("As disciplinas podem ser visualizadas", async ({ assert, client }) => {
-  const disciplina = await Disciplina.create({
-    nome: 'Matemática',
-    curso_id: 1
-  })
+test("uma disciplina em específico pode ser visualizada", async ({ assert, client }) => {
+  const disciplina = await Factory.model('App/Models/Disciplina').create();
 
   const response = await client.get('/api/disciplinas/' + disciplina.id).end()
 
-  response.assertStatus(200)
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    nome: disciplina.nome
+  });
 });
 
-test("As disciplinas podem ser visualizadas", async ({ assert, client }) => {
-  const disciplina = await Disciplina.create({
-    nome: 'Matemática',
-    curso_id: 1
-  })
+test("uma disciplina pode ser atualizada", async ({ assert, client }) => {
+  const disciplina = await Factory.model('App/Models/Disciplina').create();
 
-  const response = await client.put('/api/disciplinas/' + disciplina.id).end()
+  const { nome, curso_id } = await Factory.model('App/Models/Disciplina').make()
+  const atualizacao = { nome, curso_id };
 
-  response.assertStatus(200)
+  const response = await client.patch('/api/disciplinas/' + disciplina.id)
+    .send(atualizacao)
+    .end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset(atualizacao);
+});
+
+
+test("uma disciplina pode ser apagada", async ({ assert, client }) => {
+  const disciplina = await Factory.model('App/Models/Disciplina').create();
+
+  const response = await client.delete('/api/disciplinas/' + disciplina.id).end()
+
+  response.assertStatus(200);
+  const findDisciplina = await Disciplina.find(disciplina.id);
+  assert.isNull(findDisciplina);
 });
 
